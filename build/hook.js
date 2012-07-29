9f5f710818ae75d2421f7ad4c34d9c666033f9ea
Vivek Jain <viveksjain@gmail.com>
2012-07-29 16:32:40 +0530

    Changed modifiers to more closely match those of Chrome

--- build/hook.js
+++ build/hook.js
@@ -66,13 +66,12 @@
 
     DomUtils.prototype.simulateClick = function(element, modifiers) {
       var event, eventSequence, mouseEvent, _i, _len, _results;
-      modifiers || (modifiers = {});
       eventSequence = ["mouseover", "mousedown", "mouseup", "click"];
       _results = [];
       for (_i = 0, _len = eventSequence.length; _i < _len; _i++) {
         event = eventSequence[_i];
         mouseEvent = document.createEvent("MouseEvents");
-        mouseEvent.initMouseEvent(event, true, true, window, 1, 0, 0, 0, 0, modifiers.ctrlKey, false, false, modifiers.metaKey, 0, null);
+        mouseEvent.initMouseEvent(event, true, true, window, 1, 0, 0, 0, 0, modifiers.ctrlKey, false, modifiers.shiftKey, modifiers.metaKey, 0, null);
         _results.push(element.dispatchEvent(mouseEvent));
       }
       return _results;
@@ -124,21 +123,19 @@
       return this.update_link_focus();
     };
 
-    Application.prototype.follow_link = function(link, new_window) {
+    Application.prototype.follow_focused_link = function(ctrlOrMetaPressed, shiftPressed) {
       var modifiers;
-      if (new_window) {
+      modifiers = {};
+      if (ctrlOrMetaPressed) {
         modifiers = {
           metaKey: this.dom_utils.platform === "Mac",
           ctrlKey: this.dom_utils.platform !== "Mac"
         };
-      } else {
-        $(link).addClass("deadmouse-clicked");
       }
-      return this.dom_utils.simulateClick(link, modifiers);
-    };
-
-    Application.prototype.follow_focused_link = function(new_window) {
-      return this.follow_link(this.matched_links[this.focused_link_index], new_window);
+      if (shiftPressed) {
+        modifiers.shiftKey = true;
+      }
+      return this.dom_utils.simulateClick(this.matched_links[this.focused_link_index], modifiers);
     };
 
     Application.prototype.clear = function() {
@@ -191,11 +188,7 @@
         return false;
       } else if (this.activated && event.keyCode === 13) {
         this.clear();
-        if (event.shiftKey) {
-          this.follow_focused_link(true);
-        } else {
-          this.follow_focused_link(false);
-        }
+        this.follow_focused_link(event.ctrlKey || event.metaKey, event.shiftKey);
         this.reset();
         return false;
       }
